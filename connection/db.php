aea60d60540861c9b6aa39a38357f4981da44537
kongvut sangkla <kongvut@kkumail.com>
2015-05-22 10:54:27 +0700

    connect DB

---
+++ connection/db.php
@@ -0,0 +1,16 @@
+<?php
+
+## System Start ############################################################
+$mysqli = new mysqli("localhost", "user", "pass", "databasename");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+$mysqli ->set_charset("utf8");
+############################################################################
+
+//$sql = "SELECT * FROM v04cascap.puser WHERE pid='2048'";
+//$res = $mysqli->query($sql)or die('[' . $mysqli->error . ']');
+//$res->fetch_assoc();
+//$res->fetch_array();
+//$res->num_rows;
+?>
--- starter.php
+++ starter.php
@@ -1,12 +1,13 @@
 <?php require_once 'theme/util.inc.php'; $MasterPage = 'page_main.php';?>
 
-<?php sb('title'); echo 'สวัสดีครับ'; eb();?>
+<?php sb('title');?> สวัสดีครับ <?php eb();?>
 
 <?php sb('js_and_css_head'); ?>
 <script src=""></script>
 <?php eb();?>
 
 <?php sb('content');?>
+<?php include "connection/db.php"; ?>
         <!-- Content Header (Page header) -->
         <section class="content-header">
           <h1>
@@ -26,6 +27,7 @@
 
 
 <?php sb('js_and_css_footer');?>
+test
 <?php eb();?>
  
 <?php render($MasterPage);?>
