917f22bc5c81048724d8c73819883d049e8e0a8e
codeerror <8ohkoovd@gmail.com>
2015-05-22 11:35:16 +0700

    Edit db.

--- connection/db.php
+++ connection/db.php
@@ -1,7 +1,7 @@
 <?php
 
 ## System Start ############################################################
-$mysqli = new mysqli("localhost", "user", "pass", "databasename");
+$mysqli = new mysqli("localhost", "", "", "thaipalliative_lte");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
