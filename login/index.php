f68dac1a5a3ad83e7dcd1e31792312edbba44b78
Krit Poomthongdee <krit.ptd@gmail.com>
2015-05-22 11:52:32 +0700

    login

---
+++ login/index.php
@@ -0,0 +1,33 @@
+<?php require_once '../theme/util.inc.php'; $MasterPage = 'page_main.php';?>
+
+<?php sb('title');?> สวัสดีครับ <?php eb();?>
+
+<?php sb('js_and_css_head'); ?>
+<script src=""></script>
+<?php eb();?>
+
+<?php sb('content');?>
+<?php include "../connection/db.php"; ?>
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+          <h1>
+            Page Header
+            <small>Optional description</small>
+          </h1>
+          <p><?php //echo $_SESSION['test']; ?></p>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+          <!-- Your Page Content Here -->
+
+        </section><!-- /.content -->
+<?php eb();?>
+
+
+<?php sb('js_and_css_footer');?>
+test
+<?php eb();?>
+ 
+<?php render($MasterPage);?>
