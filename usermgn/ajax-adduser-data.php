ec0066afbae6dd47a5522e2f29a03d07faa7e683
ball69 <skaball69@gmail.com>
2015-05-25 16:17:11 +0700

    Commit User Management

--- usermgn/ajax-adduser-data.php
+++ usermgn/ajax-adduser-data.php
@@ -1,5 +1,10 @@
 <?php
-  include "../_connection/db.php"; 
+  require_once '../_theme/util.inc.php'; $MasterPage = 'page_main.php';
+  include "../_connection/db.php";
+  $status = $_SESSION["tpc_puser_status"];
+  $area = $_SESSION["tpc_puser_area"];
+  $site = $_SESSION["tpc_puser_site"];
+  $status=1;
 ?>
 <div class="row">
 <div class="col-lg-6">
@@ -37,21 +42,60 @@
 </div>
 </div>
 </div>
-<div class="row">
-    
-</div>
+
 <div class="row">
     <div class="col-lg-12">
-    <div class="form-group">
+      <div class="form-group">
         <label>สถานะ</label>
-        <select class="form-control">
-            <option value="0">- เลือกสถานะ -</option>
-            <option value="1">Super Admin</option>
-            <option value="2">Admin Area</option>
-            <option value="3">Admin Site</option>
-            <option value="4">User Site</option>
-        </select>
+          <select class="form-control" id="status" name="status">
+              <option value="0">- เลือกสถานะ -</option>
+              <?php if($status==1){?>
+              <option value="1">Super Admin</option>
+              <option value="2">Admin Area</option>
+              <option value="3">Admin Site</option>
+              <?php }if($status==2){?>
+              <option value="3">Admin Site</option>
+              <?php }?>
+              <?php if($status==3){?>
+              <option value="3">User Site</option>
+              <?php }?>
+          </select>
+      </div>
     </div>
+</div>
+<div class="row" id="moreSelect" style="display:none;">
+    <div class="col-lg-6">
+      <label>เขตบริการ</label>
+      <select class="form-control">
+        <option value="0">- เลือกเขตบริการสุขภาพ -</option>
+        <?php for($i=1;$i<=13;$i++){?>
+          <?php echo "<option value='".$i."'>เขตบริการสุขภาพที่ ".$i."</option>";?>
+        <?php } ?>
+      </select>
+    </div>
+    <div class="col-lg-6" id="moreSelect2" style="display:none;">
+      <label>หน่วยบริการ</label>
+      <select class="form-control">
+        <option ></option>
+      </select>
     </div>
 </div>
 <?php ?>
+<script>
+  $("#status").change(function(){
+      var status = $(this).val();
+      if (status==2) {
+        //code
+        $("#moreSelect2").hide();
+        $("#moreSelect").show();
+      }else if (status==3) {
+        //code
+        $("#moreSelect2").show();
+        $("#moreSelect").show();
+      }
+      else{
+        $("#moreSelect2").hide();
+        $("#moreSelect").hide();
+      }
+    });
+</script>
