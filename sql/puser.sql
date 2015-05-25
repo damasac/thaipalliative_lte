bfc57727edefc6f87a509412ad3b86d0243895b9
ppanuwat <panuwat@thaimedresnet.org>
2015-05-25 21:32:44 +0700

    new pusher

---
+++ sql/puser.sql
@@ -0,0 +1,45 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Local
+ Source Server Type    : MySQL
+ Source Server Version : 50533
+ Source Host           : localhost
+ Source Database       : thaipalliative_lte
+
+ Target Server Type    : MySQL
+ Target Server Version : 50533
+ File Encoding         : utf-8
+
+ Date: 05/25/2015 21:32:30 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `puser`
+-- ----------------------------
+DROP TABLE IF EXISTS `puser`;
+CREATE TABLE `puser` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `fname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `hcode` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `area` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `district` varchar(6) COLLATE utf8_unicode_ci NOT NULL,
+  `amphur` varchar(4) COLLATE utf8_unicode_ci NOT NULL,
+  `province` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
+  `createdate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+--  Records of `puser`
+-- ----------------------------
+INSERT INTO `puser` VALUES ('1', 'admin', '90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad', 'admin@admin.com', 'fadmin', 'ladmin', '1', '2', '3', '', '', '', '2015-05-22 15:17:01');
+
