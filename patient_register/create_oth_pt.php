b44f7144eeb6594da958f38f3eb9548e98245a44
codeerror <8ohkoovd@gmail.com>
2015-05-26 00:35:03 +0700

    Fixed.

--- patient_register/create_oth_pt.php
+++ patient_register/create_oth_pt.php
@@ -1,7 +1,90 @@
 <?php
-	$param = $_GET['mr_id'];
+	session_start();
+
+  /** Hospital code. */
+	$hospcode = $_SESSION['tpc_puser_hcode'];
+
+	$param = $_GET['ptid'];
 	include "../_connection/db.php";
-	$stmt = $mysqli->prepare('SELECT `mr_id`, `hospcode`, `pid`, `hn`, `ssn`, `birth`, `age`, `prename`, `name`, `lname`, `sex`, `race`, `nation`, `religion`, `house`, `moo`, `village`, `lane`, `road`, `tambon`, `ampur`, `changwat`, `zipcode`, `tel`, `privilege`, `mstatus`, `occupa`, `congenital_disease`, `history`, `update_time`, `update_by`, `create_time`, `create_by` FROM palliative_register WHERE mr_id = ?');
+	$stmt = $mysqli->prepare('SELECT `ptid`, `ptid_key`, `hospcode`, `pid`, `hn`, `ssn`, `birth`, `age`, `prename`, `name`, `lname`, `sex`, `race`, `nation`, `religion`, `house`, `moo`, `village`, `lane`, `road`, `tambon`, `ampur`, `changwat`, `zipcode`, `tel`, `privilege`, `mstatus`, `occupa`, `congenital_disease`, `history`, `update_time`, `update_by`, `create_time`, `create_by` FROM palliative_register WHERE ptid = ?');
 	$stmt->bind_param('s', $param);
 	$stmt->execute();
+
+	/** Get result. */  
+	$table = ""; 
+  $result = $stmt->get_result();
+  $row = mysqli_fetch_assoc($result);
+
+  $sql = "INSERT INTO 
+						palliative_register 
+						( 
+							`ptid_key`,
+							`hospcode`, 
+							`pid`, 
+							`hn`, 
+							`ssn`, 
+							`birth`, 
+							`age`, 
+							`prename`, 
+							`name`, 
+							`lname`, 
+							`sex`, 
+							`race`, 
+							`nation`, 
+							`religion`, 
+							`house`, 
+							`moo`, 
+							`village`, 
+							`lane`, 
+							`road`, 
+							`tambon`, 
+							`ampur`, 
+							`changwat`, 
+							`zipcode`, 
+							`tel`, 
+							`privilege`, 
+							`mstatus`, 
+							`occupa`, 
+							`congenital_disease`, 
+							`history`, 
+							`update_time`, 
+							`update_by`, 
+							`create_time`, 
+							`create_by`)
+						VALUES ('".$row['ptid_key']."', 
+							'".$hospcode."', 
+							'".$row['pid']."', 
+							'".$row['hn']."', 
+							'".$row['ssn']."', 
+							'".$row['birth']."', 
+							'".$row['age']."', 
+							'".$row['prename']."', 
+							'".$row['name']."', 
+							'".$row['lname']."', 
+							'".$row['sex']."', 
+							'".$row['race']."', 
+							'".$row['nation']."', 
+							'".$row['religion']."', 
+							'".$row['house']."', 
+							'".$row['moo']."', 
+							'".$row['village']."', 
+							'".$row['lane']."', 
+							'".$row['road']."', 
+							'".$row['tambon']."', 
+							'".$row['ampur']."', 
+							'".$row['changwat']."', 
+							'".$row['zipcode']."', 
+							'".$row['tel']."', 
+							'".$row['privilege']."', 
+							'".$row['mstatus']."', 
+							'".$row['occupa']."', 
+							'".$row['congenital_disease']."', 
+							'".$row['history']."', 
+							'".$row['update_time']."', 
+							'".$row['update_by']."', 
+							'".$row['create_time']."', 
+							'".$row['create_by']."')";
+  	$mysqli->query($sql);
+	//echo $sql;
+	header( "location: index.php" );
 ?>
--- patient_register/search_ssn.php
+++ patient_register/search_ssn.php
@@ -2,12 +2,15 @@
   session_start();
 
   /** Search from site user. */
-	$hospcode = $_SESSION['tpc_puser_area'];
+	$hospcode = $_SESSION['tpc_puser_hcode'];
 	isset($_POST['ssn']) ? $param = "%{$_POST['ssn']}%" : $param = '';
 	include "../_connection/db.php";
-	$stmt = $mysqli->prepare('SELECT `mr_id`, `hospcode`, `pid`, `hn`, `ssn`, `birth`, `age`, `prename`, `name`, `lname`, `sex`, `race`, `nation`, `religion`, `house`, `moo`, `village`, `lane`, `road`, `tambon`, `ampur`, `changwat`, `zipcode`, `tel`, `privilege`, `mstatus`, `occupa`, `congenital_disease`, `history`, `update_time`, `update_by`, `create_time`, `create_by` FROM palliative_register WHERE ssn LIKE ? AND hospcode = ?');
+	$stmt = $mysqli->prepare('SELECT `ptid`, `ptid_key`, `hospcode`, `pid`, `hn`, `ssn`, `birth`, floor(datediff(curdate(),birthdb)/365.25) as age, `prename`, `name`, `lname`, `sex`, `race`, `nation`, `religion`, `house`, `moo`, `village`, `lane`, `road`, `tambon`, `ampur`, `changwat`, `zipcode`, `tel`, `privilege`, `mstatus`, `occupa`, `congenital_disease`, `history`, `update_time`, `update_by`, `create_time`, `create_by` FROM palliative_register WHERE ssn LIKE ? AND hospcode = ?');
 	$stmt->bind_param('ss', $param, $hospcode);
 	$stmt->execute();
+
+  $sex[1]="ชาย";
+  $sex[2]="หญิง";
 
   /** init table. */
 	$javascript = '<script>$(function () {
@@ -26,13 +29,12 @@
 	$table = '<table id="example2" class="table table-bordered table-hover">
                         <thead>
                           <tr>
-                            <th>Hospital code</th>
-			                <th>PID</th>
+                            <th>HCODE</th>
+			                <th>PTCODE</th>
 			                <th>HN</th>
-			                <th>เลขบัตรประจำตัวประชาชน</th>
 			                <th>ชื่อ</th>
-			                <th>นามสกุล</th>
-			                <th>วันเกิด</th>
+			                <th>สกุล</th>
+			                <th>เพศ</th>
 			                <th>อายุ</th>
 			                <th></th>
                           </tr>
@@ -50,16 +52,12 @@
                   <td>'.$row['hospcode'].'</td>
                   <td>'.$row['pid'].'</td>
                   <td>'.$row['hn'].'</td>
-                  <td>'.$row['ssn'].'</td>
                   <td>'.$row['name'].'</td>
                   <td>'.$row['lname'].'</td>
-                  <td>'.$row['birth'].'</td>
-                  <td>'.$row['birth'].'</td>
+                  <td>'.$sex[$row['sex']].'</td>
+                  <td>'.$row['age'].'</td>
                   <td>
-                  	<button type="button" class="btn btn-default" title="ดูข้อมูล">
-  	                	<i class="fa fa-eye">
-  	                	</i>
-                  	</button>
+                  	<a href="../emr/?ptid_key='.$row['ptid_key'].'" class="btn btn-block btn-success">EMR</a>
                   </td>
                 </tr>';
   	}
@@ -69,7 +67,7 @@
                 </table>';
   }else{
     /** Search from other site. */
-    $stmt = $mysqli->prepare('SELECT `mr_id`, `hospcode`, `pid`, `hn`, `ssn`, `birth`, `age`, `prename`, `name`, `lname`, `sex`, `race`, `nation`, `religion`, `house`, `moo`, `village`, `lane`, `road`, `tambon`, `ampur`, `changwat`, `zipcode`, `tel`, `privilege`, `mstatus`, `occupa`, `congenital_disease`, `history`, `update_time`, `update_by`, `create_time`, `create_by` FROM palliative_register WHERE ssn LIKE ?');
+    $stmt = $mysqli->prepare('SELECT `ptid`, `hospcode`, `pid`, `hn`, `ssn`, `birth`, `age`, `prename`, `name`, `lname`, `sex`, `race`, `nation`, `religion`, `house`, `moo`, `village`, `lane`, `road`, `tambon`, `ampur`, `changwat`, `zipcode`, `tel`, `privilege`, `mstatus`, `occupa`, `congenital_disease`, `history`, `update_time`, `update_by`, `create_time`, `create_by` FROM palliative_register WHERE ssn LIKE ?');
     $stmt->bind_param('s', $param);
     $stmt->execute();
 
@@ -90,10 +88,10 @@
                     <td>'.$row['birth'].'</td>
                     <td>'.$row['birth'].'</td>
                     <td>
-                      <button type="button" class="btn btn-default" title="บันทึกข้อมูลจากโรงพยาบาลอื่น">
+                      <a type="button" href="create_oth_pt.php?ptid='.$row['ptid'].'" class="btn btn-default" title="บันทึกข้อมูลจากโรงพยาบาลอื่น">
                         <i class="fa fa-arrow-down">
                         </i>
-                      </button>
+                      </a>
                     </td>
                   </tr>';
       }
