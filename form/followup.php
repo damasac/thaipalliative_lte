e774ffdd264b33f2ef20d19759807c2d0dffdb52
Krit Poomthongdee <krit.ptd@gmail.com>
2015-05-26 20:52:42 +0700

    form3+sql

--- form/followup.php
+++ form/followup.php
@@ -1,6 +1,6 @@
 <?php require_once '../_theme/util.inc.php'; $MasterPage = 'page_main.php';?>
 
-<?php sb('title');?> Register <?php eb();?>
+<?php sb('title');?> Follow up <?php eb();?>
 
 <?php sb('js_and_css_head'); ?>
 
@@ -17,7 +17,24 @@
 <?php eb();?>
 
 <?php sb('content');?>
-<?php include_once "../_connection/db.php"; ?>
+<?php
+include_once "../_connection/db.php";
+
+$res = $mysqli->query("SELECT * FROM palliative_followup")or die('[' . $mysqli->error . ']');
+$numGroup = $res->num_rows;
+$fields = $res->fetch_fields();
+//echo "<pre>"; echo $fields[1]->name; echo "</pre>";
+$sql = "SELECT * FROM palliative_followup WHERE ptid = '$_GET[dataid]';";
+$res = $mysqli->query($sql)or die('[' . $mysqli->error . ']');
+$dbarr = $res->fetch_array();
+  
+//echo "<pre>".$sql; print_r($dbarr); echo "</pre>";
+if($dbarr['ptid']){
+    $task = 'update';
+}else {
+    $task = 'save';
+}
+?>
 
    <div class="info-box">
     <span class="info-box-icon btn-foursquare" style="background-color: #3C8DBC;"><i class="fa fa-user-plus"></i></span>
@@ -37,12 +54,12 @@
 
         <div class="form-group col-lg-6">
           <label>HOSPCODE: </label>
-          <input type="text" name="" class="form-control" id="" value="">
+          <input type="text" name="<?php $i=1; echo $fields[$i]->name;?>" class="form-control" id="<?php echo $fields[$i]->name;?>" value="<?php echo $dbarr[$i]; ?>">
         </div>
         
         <div class="form-group col-lg-6">
           <label>PID: </label>
-          <input type="text" name="" class="form-control" id="" value="">
+          <input type="text" name="<?php $i++; echo $fields[$i]->name;?>" class="form-control" id="<?php echo $fields[$i]->name;?>" value="<?php echo $dbarr[$i]; ?>">
         </div>
 
         <div class="form-group col-lg-12">
@@ -51,8 +68,8 @@
         
         <div class="form-group col-lg-12">
           <div class='showForm'><label>1. กิจกรรมการดูแล (เลือกได้มากกว่า 1 ตัวเลือก)</label><br>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='1'>การจัดการอาการด้วย Morphine</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='2'>Family metting</label></div>
+            <div class='checkbox-inline'><label><input type='checkbox' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='1'>การจัดการอาการด้วย Morphine</label></div>
+            <div class='checkbox-inline'><label><input type='checkbox' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='2'>Family metting</label></div>
           </div>
         </div>
 
@@ -62,91 +79,91 @@
         
         <div class="form-group col-lg-12">
           <div class='showForm'><label>2. การพูดคุยเรื่องสถานที่ดูแล</label><br>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='1'>บ้าน</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='2'>รพ.ใกล้บ้าน</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='3'>รพ.ศรีนครินทร์</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='1'>บ้าน</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='2'>รพ.ใกล้บ้าน</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='3'>รพ.ศรีนครินทร์</label></div>
           </div>
         </div>
 
         <div class="form-group col-lg-12">
           <div class='showForm'><label>3. การพูดคุยเรื่องสถานที่เสียชีวิต</label><br>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='1'>บ้าน</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='2'>รพ.ใกล้บ้าน</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='3'>รพ.ศรีนครินทร์</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='1'>บ้าน</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='2'>รพ.ใกล้บ้าน</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='3'>รพ.ศรีนครินทร์</label></div>
           </div>
         </div>
 
         <div class="form-group col-lg-12">
           <div class='showForm'><label>4. การพูดคุยเรื่องการปฏิเสธเครื่องพยุงชีพ</label><br>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='1'>บ้าน</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='2'>รพ.ใกล้บ้าน</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='3'>รพ.ศรีนครินทร์</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='1'>บ้าน</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='2'>รพ.ใกล้บ้าน</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='3'>รพ.ศรีนครินทร์</label></div>
           </div>
         </div>
 
         <div class="form-group col-lg-12">
           <div class='showForm'><label>-> Livings will</label><br>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='1'>Document</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='2'>Verbal</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='1'>Document</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='2'>Verbal</label></div>
           </div>
         </div>
 
         <div class="form-group col-lg-12">
           <div class='showForm'><label>5. ได้รับการพยุงชีพ (ใส่ ETT) ก่อนปรึกษา</label><br>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='1'>Yes</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='2'>No</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='1'>Yes</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='2'>No</label></div>
           </div>
         </div>
 
         <div class="form-group col-lg-12">
           <div class='showForm'><label>6. การถอดถอนเครื่องพยุงชีพ</label><br>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='1'>ETT</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='2'>Intrope</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='3'>ATB</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='4'>Nutrition</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='5'>Hydration</label></div>
+            <div class='checkbox-inline'><label><input type='checkbox' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='1'>ETT</label></div>
+            <div class='checkbox-inline'><label><input type='checkbox' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='2'>Intrope</label></div>
+            <div class='checkbox-inline'><label><input type='checkbox' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='3'>ATB</label></div>
+            <div class='checkbox-inline'><label><input type='checkbox' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='4'>Nutrition</label></div>
+            <div class='checkbox-inline'><label><input type='checkbox' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='5'>Hydration</label></div>
           </div>
         </div>
 
         <div class="form-group col-lg-6">
           <label>7. วันที่เสียชีวิต </label>
-          <input type="date" name="" class="form-control" id="" value="">
+          <input type="date" name="<?php $i++; echo $fields[$i]->name;?>" class="form-control" id="<?php echo $fields[$i]->name;?>" value="<?php echo $dbarr[$i]; ?>">
         </div>
 
         <div class="form-group col-lg-12">
           <div class='showForm'><label>สถานที่เสียชีวืต</label><br>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='1'>บ้าน</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='2'>รพ.ใกล้บ้าน</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='3'>รพ.ศรีนครินทร์</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='1'>บ้าน</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='2'>รพ.ใกล้บ้าน</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='3'>รพ.ศรีนครินทร์</label></div>
           </div>
         </div>
 
         <div class="form-group col-lg-12">
           <div class='showForm'><label>ลักษณะการเสียชีวิต</label><br>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='1'>สงบ</label></div>
-            <div class='checkbox-inline'><label><input type='checkbox' name="" id="" value='2'>ไม่สงบ</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php $i++; echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='1'>สงบ</label></div>
+            <div class='radio-inline'><label><input type='radio' name="<?php echo $fields[$i]->name;?>" id="<?php echo $fields[$i]->name;?>" <?php echo $dbarr[$i]  =='1' ? 'checked' : ''; ?> value='2'>ไม่สงบ</label></div>
           </div>
         </div>
 
         <div class="form-group col-lg-12">
           <label>Problem list</label>
-          <textarea class="form-control"></textarea>
+          <textarea name="<?php $i++; echo $fields[$i]->name;?>" class="form-control" id="<?php echo $fields[$i]->name;?>" value="<?php echo $dbarr[$i]; ?>"></textarea>
         </div>
 
         <div class="form-group col-lg-12">
-          <label></label>
+              <label></label>
             <div class="row">
               <div class="col-lg-4">
-                Form completed by<input type="text" name="" class="form-control">
+                Form completed by<input type="text" name="<?php $i++; echo $fields[$i]->name;?>" value="<?php echo $dbarr[$i]; ?>" class="form-control">
               </div>
               <div class="col-lg-4">
-                รหัสที่ได้จากระบบ<input type="text" name="" class="form-control">
+                รหัสที่ได้จากระบบ<input type="text" name="<?php $i++; echo $fields[$i]->name;?>" value="<?php echo $dbarr[$i]; ?>" class="form-control">
               </div>
               <div class="col-lg-4">
-                Date<input type="date" name="" class="form-control">
+                Date<input type="date" name="<?php $i++; echo $fields[$i]->name;?>" value="<?php echo $dbarr[$i]; ?>" class="form-control">
               </div>
               </div><hr>
-            </div>
+        </div>
             
             
             <div class="form-group col-lg-6">
--- sql/palliative_followup.sql
+++ sql/palliative_followup.sql
@@ -1,47 +1,66 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.4.3
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 26, 2015 at 03:49 PM
+-- Server version: 5.6.24
+-- PHP Version: 5.5.24
 
-Source Server         : localhost
-Source Server Version : 50617
-Source Host           : localhost:3306
-Source Database       : thaipalliative_lte
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50617
-File Encoding         : 65001
 
-Date: 2015-05-26 13:46:04
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Database: `thaipalliative_lte`
+--
 
--- ----------------------------
--- Table structure for palliative_followup
--- ----------------------------
-DROP TABLE IF EXISTS `palliative_followup`;
-CREATE TABLE `palliative_followup` (
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `palliative_followup`
+--
+
+CREATE TABLE IF NOT EXISTS `palliative_followup` (
   `ptid` bigint(20) NOT NULL COMMENT 'ID',
   `hospcode` varchar(15) NOT NULL COMMENT 'Hospital Code',
   `pid` varchar(15) NOT NULL COMMENT 'PID',
-  `hn` varchar(15) NOT NULL COMMENT 'HN',
   `f3value1` int(11) DEFAULT NULL COMMENT 'กิจกรรมการดูแล',
+  `f3value1_1` int(11) NOT NULL COMMENT 'กิจกรรมการดูแล',
   `f3value2` int(11) DEFAULT NULL COMMENT 'การพูดคุยเรื่องสถานที่ดูแล',
   `f3value3` int(11) DEFAULT NULL COMMENT 'การพูดคุยเรื่องสถานที่เสียชีวิต',
   `f3value4` int(11) DEFAULT NULL COMMENT 'การพูดคุยเรื่องการปฏิเสธเครื่องพยุงชีพ',
   `f3value4_2` int(11) DEFAULT NULL COMMENT 'Livings will',
   `f3value5` int(11) DEFAULT NULL COMMENT 'ได้รับการพยุงชีพ (ใส่ ETT) ก่อนปรึกษา',
   `f3value6` int(11) DEFAULT NULL COMMENT 'การถอดถอนเครื่องพยุงชีพ',
+  `f3value6_2` int(11) NOT NULL COMMENT 'การถอดถอนเครื่องพยุงชีพ',
+  `f3value6_3` int(11) NOT NULL COMMENT 'การถอดถอนเครื่องพยุงชีพ',
+  `f3value6_4` int(11) NOT NULL COMMENT 'การถอดถอนเครื่องพยุงชีพ',
+  `f3value6_5` int(11) NOT NULL COMMENT 'การถอดถอนเครื่องพยุงชีพ',
   `f3datedeath` date DEFAULT NULL COMMENT 'วันที่เสียชีวิต',
   `f3deathlocation` int(11) DEFAULT NULL COMMENT 'สถานที่เสียชีวิต',
-  `f3deathreason` varchar(255) DEFAULT NULL COMMENT 'ลักษณะการเสียชีวิต',
+  `f3deathreason` int(11) DEFAULT NULL COMMENT 'ลักษณะการเสียชีวิต',
   `f3problemlist` text COMMENT 'Problem list',
-  `update_time` datetime NOT NULL COMMENT 'แก้ไขเวลา',
   `update_by` int(11) NOT NULL COMMENT 'แก้ไขโดย',
-  `create_time` datetime NOT NULL COMMENT 'สร้างเวลา',
   `create_by` int(11) NOT NULL COMMENT 'สร้างโดย',
-  PRIMARY KEY (`ptid`)
+  `update_time` datetime NOT NULL COMMENT 'แก้ไขเวลา'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ทะเบียนผู้ป่วย';
 
--- ----------------------------
--- Records of palliative_followup
--- ----------------------------
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `palliative_followup`
+--
+ALTER TABLE `palliative_followup`
+  ADD PRIMARY KEY (`ptid`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
