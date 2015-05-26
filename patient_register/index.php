3673776fa333f6b4b98c8879385fab3f0e5bb95b
ppanuwat <panuwat@thaimedresnet.org>
2015-05-26 13:58:35 +0700

    add bed icon

--- patient_register/index.php
+++ patient_register/index.php
@@ -25,7 +25,7 @@
     <div class="info-box-content">
       <span class="info-box-text"><h4>ลงทะเบียน</h4></span>
       <span class="info-box-text">
-          <a class="btn btn-default" id="user" href="search.php"><strong>เพิ่มข้อมูล</strong></a>
+          <a class="btn btn-default" id="user" href="search.php"><i class="fa fa-fw fa-bed"></i>  <strong>เพิ่มข้อมูลผู้ป่วยใหม่</strong></a>
       </span>
     </div>
   </div>
