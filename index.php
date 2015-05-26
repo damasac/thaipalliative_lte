b69f31971bf66c3c37f071eb6b0067ba6ada1341
kongvut sangkla <kongvut@kkumail.com>
2015-05-26 10:47:06 +0700

    fix index

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
-if ($_SESSION['tpc_puser_hcode'] != "") {
+header("Content-type:text/html; charset=UTF-8");
+
+if (isset($_SESSION['tpc_puser_hcode'])) {
   Header("Location: home.php");
   exit;
 }
 ?>
-<?php header("Content-type:text/html; charset=UTF-8"); ?>
+
 <!DOCTYPE html>
 <html>
   <head>
